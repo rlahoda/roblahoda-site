3bde28aec4b3781a8faa772f8ef685a385a9b542
Rob Lahoda <rob@roblahoda.com>
2020-01-17 09:51:45 -0500

    updating about template and config

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -1,55 +1,76 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { graphql } from 'gatsby'
-import Layout from '../components/Layout'
-import Content, { HTMLContent } from '../components/Content'
+import React from "react";
+import PropTypes from "prop-types";
+import { graphql } from "gatsby";
+import Layout from "../components/Layout";
+import Content, { HTMLContent } from "../components/Content";
+import { withPrefix } from "gatsby";
 
-export const AboutPageTemplate = ({ title, content, contentComponent }) => {
-  const PageContent = contentComponent || Content
+export const AboutPageTemplate = ({
+  title,
+  subtitle,
+  aboutPhoto,
+  content,
+  contentComponent
+}) => {
+  const PageContent = contentComponent || Content;
 
   return (
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className="section">
-              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                {title}
-              </h2>
-              <PageContent className="content" content={content} />
-            </div>
-          </div>
+    <main>
+      <div className="pageHeading">
+        <h1 className="bigTitle">{title}</h1>
+        <div className="pageTagline__container">
+          <h2 id="projectsTagline" className="pageTagline">
+            {subtitle}
+          </h2>
         </div>
       </div>
-    </section>
-  )
-}
+
+      <div className="aboutContainer">
+        <div className="infoCard aboutCard aboutInfoMain">
+          <PageContent content={content} />
+          <img
+            className="aboutPicture"
+            src={`${withPrefix("/")}${aboutPhoto}`}
+            alt="A photo of me"
+          />
+        </div>
+      </div>
+    </main>
+  );
+};
 
 AboutPageTemplate.propTypes = {
   title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string,
+  aboutPhoto: PropTypes.string,
   content: PropTypes.string,
-  contentComponent: PropTypes.func,
-}
+  contentComponent: PropTypes.func
+};
 
 const AboutPage = ({ data }) => {
-  const { markdownRemark: post } = data
+  const { markdownRemark: post } = data;
 
   return (
     <Layout>
+      <header>
+        <div className="pageTopImage"></div>
+      </header>
       <AboutPageTemplate
         contentComponent={HTMLContent}
         title={post.frontmatter.title}
         content={post.html}
+        subtitle={post.frontmatter.subtitle}
+        aboutPhoto={post.frontmatter.aboutPhoto}
       />
     </Layout>
-  )
-}
+  );
+};
 
 AboutPage.propTypes = {
-  data: PropTypes.object.isRequired,
-}
+  data: PropTypes.object.isRequired
+};
 
-export default AboutPage
+export default AboutPage;
 
 export const aboutPageQuery = graphql`
   query AboutPage($id: String!) {
@@ -57,7 +78,9 @@ export const aboutPageQuery = graphql`
       html
       frontmatter {
         title
+        subtitle
+        aboutPhoto
       }
     }
   }
-`
+`;
