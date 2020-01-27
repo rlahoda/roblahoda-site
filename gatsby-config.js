a0f54e3d5680d2e41c97c1bd7ed98a02f76ed996
Rob Lahoda <rob@roblahoda.com>
2020-01-26 20:21:20 -0500

    COnfig update

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
     {
       resolve: "gatsby-source-filesystem",
       options: {
-        path: `${__dirname}/src/img`,
+        path: `${__dirname}/src/assets/images`,
         name: "images"
       }
     },
